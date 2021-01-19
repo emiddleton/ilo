00fc4ec113cebb9c148230a1ea1507f69d90b8de
Edward Middleton <edward.middleton@vortorus.net>
2021-01-20 02:14:57 +0900

    cleanup unneeded command line arguments

--- ilo_ribcl_derive/src/ribcl_auth.rs
+++ ilo_ribcl_derive/src/ribcl_auth.rs
@@ -6,7 +6,7 @@ pub fn impl_ribcl_auth_macro(item_struct: ItemStruct) -> syn::Result<TokenStream
     let mut item_struct = item_struct;
     if let syn::Fields::Named(ref mut fields) = item_struct.fields {
         fields.named.push(syn::Field::parse_named.parse2(quote! {
-            /// don't update or create endpoint file
+            /// don't update endpoint file
             #[structopt(short, long)]
             pub no_update: bool
         })?);
@@ -19,16 +19,6 @@ pub fn impl_ribcl_auth_macro(item_struct: ItemStruct) -> syn::Result<TokenStream
             // endpoint info file
             #[structopt(short, long, parse(from_os_str), default_value = "endpoint.json")]
             pub endpoint: std::path::PathBuf
-        })?);
-        fields.named.push(syn::Field::parse_named.parse2(quote! {
-            /// auth info file
-            #[structopt(short, long, parse(from_os_str), default_value = "auth.json")]
-            pub auth: std::path::PathBuf
-        })?);
-        fields.named.push(syn::Field::parse_named.parse2(quote! {
-            /// firmware info file
-            #[structopt(short, long, parse(from_os_str), default_value = "firmware.json")]
-            pub firmware: std::path::PathBuf
         })?);
     }
     Ok(quote! {
